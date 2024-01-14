b4cc13f181475eda8a8e486edf68006a3678643e
harsav360 <annu7418@gmail.com>
2024-01-14 13:33:02 +0530

    Context API Done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+import React from "react";
+import Header from "./components/Header";
+import Pagination from "./components/Pagination";
+import Blogs from "./components/Blogs";
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Header />
+      <Blogs />
+      <Pagination />
+    </div>
+  );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <header>Tech Blog</header>
+    </div>
+  );
+};
+
+export default Header;
